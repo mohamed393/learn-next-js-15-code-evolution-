4ece3d48ef5916ac47fe5d097c8c73d0fc125b40
MohamedMosleh <mohamed.mosleh@hypercell.net>
2026-03-17 13:25:36 +0200

    multi layouts

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,12 +1,25 @@
 // app/(app)/layout.tsx
+'use client';
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+const navLinks =[
+    {name:'Customer',href:'/marketing/customers'},
+    {name:'Revenue',href:'/marketing/revenue'},
+]
 export default function AppLayout({
                                       children,
                                   }: {
     children: React.ReactNode
 }) {
+    const pathName=usePathname();
     return (
         <div className="min-h-screen">
             <aside>Sidebar</aside>
+            {navLinks.map(link=>(
+                <Link className={`flex cursor-pointer flex-col ${pathName===link.href?'text-blue-500':'text-gray-500'}`} key={link.href} href={link.href}>{link.name}</Link>
+            ))}
+            {/*<Link  className='cursor-pointer underline text-blue-500' href='/marketing/customers'>Customers</Link>*/}
+            {/*<Link className='cursor-pointer underline text-blue-500' href='/marketing/revenue'>Revenue</Link>*/}
             <main>{children}</main>
         </div>
     )
--- app/(app)/marketing/customers/page.tsx
+++ app/(app)/marketing/customers/page.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
+
 export default function Customers(){
     return (<div>
         Customers
+        <Link className='cursor-pointer underline text-blue-500' href='/'>Home</Link>
     </div>)
 }
